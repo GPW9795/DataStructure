527d14ea3682358f1563a012761571897dc62999
gao peiwen <vivian9795@163.com>
2020-09-29 18:29:28 +0800

    00-Test

--- 00-Test/src/com/mj/Main.java
+++ 00-Test/src/com/mj/Main.java
@@ -1,9 +1,17 @@
 package com.mj;
 
+import java.util.List;
+
+/**
+ * main函数 -> main方法
+ */
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Hello World");
 
+        List list = null;
     }
 
 }
+
