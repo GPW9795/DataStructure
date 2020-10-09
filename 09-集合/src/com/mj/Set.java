2b8d739ced0bdd6f6c6a84b5949b2ded682bb3fd
gao peiwen <vivian9795@163.com>
2020-10-09 11:00:37 +0800

    集合

---
+++ 09-集合/src/com/mj/Set.java
@@ -0,0 +1,23 @@
+package com.mj;
+
+public interface Set<E> {
+    int size();
+
+    boolean isEmpty();
+
+    void clear();
+
+    boolean contains(E element);
+
+    void add(E element);
+
+    void remove(E element);
+
+    void traversal(Vistor<E> vistor);
+
+    public static abstract class Vistor<E> {
+        boolean stop;
+
+        public abstract boolean visit(E element);
+    }
+}
