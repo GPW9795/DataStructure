d1855fe1763fa3f7dd91b3c641615e31c223dcb7
gaopeiwen <vivian9795@163.com>
2020-10-10 14:38:19 +0800

    哈希表

---
+++ 11-哈希表/src/com/mj/map/HashMap.java
@@ -0,0 +1,105 @@
+package com.mj.map;
+
+public class HashMap<K, V> implements Map<K, V> {
+
+    private static final boolean RED = false;
+    private static final boolean BLACK = true;
+    private int size;
+    private Node<K, V>[] table;
+    private static final int DEFAULT_CAPACITY = 1 << 4;
+
+    public HashMap() {
+        table = new Node[DEFAULT_CAPACITY];
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public void clear() {
+        if (size == 0) return;
+        size = 0;
+        for (int i = 0; i < table.length; i++) {
+            table[i] = null;
+        }
+    }
+
+    @Override
+    public V put(K key, V value) {
+        return null;
+    }
+
+    @Override
+    public V get(K key) {
+        return null;
+    }
+
+    @Override
+    public V remove(K key) {
+        return null;
+    }
+
+    @Override
+    public boolean containsKey(K key) {
+        return false;
+    }
+
+    @Override
+    public boolean containsValue(V value) {
+        return false;
+    }
+
+    @Override
+    public void traversal(Visitor<K, V> visitor) {
+
+    }
+
+    private static class Node<K, V> {
+        K key;
+        V value;
+        boolean color = RED;
+        Node<K, V> left;
+        Node<K, V> right;
+        Node<K, V> parent;
+
+        public Node(K key, V value, Node<K, V> parent) {
+            this.key = key;
+            this.value = value;
+            this.parent = parent;
+        }
+
+        // 判断是否为叶子结点
+        public boolean isLeaf() {
+            return left == null && right == null;
+        }
+
+        public boolean hasTwoChildren() {
+            return left != null && right != null;
+        }
+
+        public boolean isLeftChild() {
+            return parent != null && this == parent.left;
+        }
+
+        public boolean isRightChild() {
+            return parent != null && this == parent.right;
+        }
+
+        public Node<K, V> sibling() {
+            if (isLeftChild()) {
+                return parent.right;
+            }
+            if (isRightChild()) {
+                return parent.left;
+            }
+            return null;
+        }
+    }
+}
